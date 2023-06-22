191ada95a2fedd80a8dc5468ba0d969dbec81cdc
Calvin Le <email.calvinle@gmail.com>
2023-06-21 19:06:21 -0700

    review seeders done

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -5,7 +5,7 @@ from datetime import datetime
 def seed_reviews():
     review1_owner_1 = Review(
         ownerId=1,
-        recipeId=1,
+        recipeId=8,
         review='This was just amazing and fun to make!',
         stars=5
     )
@@ -23,7 +23,7 @@ def seed_reviews():
     )
     review4_owner_1 = Review(
         ownerId=1,
-        recipeId=4,
+        recipeId=9,
         review='A simple recipe that can be made on the fly.',
         stars=4
     )
@@ -38,12 +38,6 @@ def seed_reviews():
         recipeId=6,
         review='Not really feeling this sandwich',
         stars=2
-    )
-    review7_owner_1 = Review(
-        ownerId=1,
-        recipeId=7,
-        review='Quick and easy to make!',
-        stars=5
     )
     
     review1_owner_2 = Review(
@@ -60,7 +54,7 @@ def seed_reviews():
     )
     review3_owner_2 = Review(
         ownerId=2,
-        recipeId=5,
+        recipeId=1,
         review='Sloppy Joes I loved making this for my kids!',
         stars=5
     )
@@ -78,32 +72,81 @@ def seed_reviews():
     )
     review6_owner_2 = Review(
         ownerId=2,
-        recipeId=8,
+        recipeId=5,
         review='Better than the restaurants!',
         stars=5
     )
-    review7_owner_2 = Review(
-        ownerId=2,
-        recipeId=9,
-        review='So yummy had this for dinner.',
-        stars=5
+
+
+    review1_owner_3 = Review(
+        ownerId=3,
+        recipeId=1,
+        review='I absolutely loved this!',
+        stars=4
+    )
+    review2_owner_3 = Review(
+        ownerId=3,
+        recipeId=2,
+        review='Crepes are my favorite!',
+        stars=4
+    )
+    review3_owner_3 = Review(
+        ownerId=3,
+        recipeId=4,
+        review="A taste of Hawaiian Food!",
+        stars=4
+    )
+    review4_owner_3 = Review(
+        ownerId=3,
+        recipeId=5,
+        review='A bit not seasoned enough for my taste.',
+        stars=2
+    )
+    review5_owner_3 = Review(
+        ownerId=3,
+        recipeId=7,
+        review='Little ingredients and easy to make!',
+        stars=3
+    )
+    review6_owner_3 = Review(
+        ownerId=3,
+        recipeId=8,
+        review='Never made korean food before and this was a great first recipe!',
+        stars=4
     )
 
-    # User 1 Reviews 1-7
+    
+    # User 1 Reviews
     db.session.add(review1_owner_1)
     db.session.add(review2_owner_1)
     db.session.add(review3_owner_1)
     db.session.add(review4_owner_1)
     db.session.add(review5_owner_1)
     db.session.add(review6_owner_1)
-    db.session.add(review7_owner_1)
 
-    # User 2 Reviews 3-9
+
+    # User 2 Reviews
     db.session.add(review1_owner_2)
     db.session.add(review2_owner_2)
     db.session.add(review3_owner_2)
     db.session.add(review4_owner_2)
     db.session.add(review5_owner_2)
     db.session.add(review6_owner_2)
-    db.session.add(review7_owner_2)
     
+    # User 3 Reviews
+    db.session.add(review1_owner_3)
+    db.session.add(review2_owner_3)
+    db.session.add(review3_owner_3)
+    db.session.add(review4_owner_3)
+    db.session.add(review5_owner_3)
+    db.session.add(review6_owner_3)
+
+    db.session.commit()
+
+    def undo_reviews():
+        if environment == "production":
+            db.session.execute(f"TRUNCATE table {SCHEMA}.reviews RESTART IDENTITY CASCADE;")
+        else:
+            db.session.execute(text("DELETE FROM reviews"))
+            
+        db.session.commit()
