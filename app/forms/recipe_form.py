467795765bf5a54f5e3c6534a7e88495245859f0
Calvin Le <email.calvinle@gmail.com>
2023-06-24 13:05:50 -0700

    recipe form done

--- app/forms/recipe_form.py
+++ app/forms/recipe_form.py
@@ -1,6 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField, TextAreaField, FileField, FormField, SubmitField
+from wtforms import StringField, TextAreaField, FormField, FieldList, SubmitField
 from wtforms.validators import DataRequired
 
+from direction_form import DirectionForm
+from ingredient_form import IngredientForm
+from recipe_image_form import RecipeImageForm
+from time_form import TimeForm
+
 class RecipeForm(FlaskForm):
-    pass
+    name = StringField('Name', validators=[DataRequired()])
+    ingredients = FieldList(IngredientForm(), min_entries=1)
+    directions = FieldList(DirectionForm(), min_entries=1)
+    time = FieldList(TimeForm(), min_entries=1)
+    preview_image = StringField('Preview Image', validators=[DataRequired()])
+    recipe_image = FieldList(FormField(RecipeImageForm), min_entries=1)
+    description = TextAreaField('Description')
+    submit = SubmitField('Submit')
