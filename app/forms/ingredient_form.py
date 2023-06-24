1805ade19c132f7f1e0fbc607a1cd878bf2ff2ba
Calvin Le <email.calvinle@gmail.com>
2023-06-24 12:53:48 -0700

    ingredient form done

---
+++ app/forms/ingredient_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, BooleanField
+from wtforms.validators import DataRequired
+
+class IngredientForm(FlaskForm):
+    id = IntegerField('ID', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    quantity = IntegerField('Quantity', validators=[DataRequired()])
+    measurement = StringField('Measurement', validators=[DataRequired()])
+    is_seasoning = BooleanField('Is Seasoning')
---
+++ app/forms/recipe_form.py
@@ -0,0 +1,6 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, BooleanField, TextAreaField, FileField, FormField, SubmitField
+from wtforms.validators import DataRequired
+
+class RecipeForm(FlaskForm):
+    
