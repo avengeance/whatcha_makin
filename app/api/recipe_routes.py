6b8d322fa09a1fcd91ade340508e8969ecdc03da
Calvin Le <email.calvinle@gmail.com>
2023-06-27 22:07:19 -0700

    refining create recipe route

--- app/api/recipe_routes.py
+++ app/api/recipe_routes.py
@@ -14,6 +14,7 @@ from flask import Blueprint, redirect, url_for, render_template, jsonify, reques
 from flask_login import login_required, current_user, logout_user
 
 from statistics import mean
+from datetime import datetime
 
 import json
 
@@ -162,45 +163,101 @@ def create_recipe():
     
     if form.validate_on_submit():
         name = form.name.data
-        ingredients = form.ingredients.data
-        directions = form.directions.data
-        time = form.time.data
-        preview_image = form.preview_image.data
-        recipe_image = form.recipe_image.data
         description = form.description.data
+        # ingredient_name = form.ingredient_name.data
+        # ingredient_quantity = form.ingredient_quantity.data
+        # ingredient_measurement = form.ingredient_measurement.data
+        # ingredient_seasoning = form.ingredient_seasoning.data
+        # direction_step = form.direction_step.data
+        # direction_step_info = form.direction_step_info.data
+        prep_time = form.prep_time.data
+        cook_time = form.cook_time.data
+        servings = form.servings.data
+        preview_image_url = form.preview_image.data
+        recipe_image_url = form.recipe_image.data
+        # ingredients = form.ingredients.data
+        # directions = form.directions.data
+        # time = form.time.data
+        # preview_image = form.preview_image.data
+        # recipe_image = form.recipe_image.data
+        # description = form.description.data
         
         new_recipe = Recipe(
             owner_id=current_user.id,
             name=name,
-            ingredients=ingredients,
-            directions=directions,
-            time=time,
-            preview_image=preview_image,
-            recipe_image=recipe_image,
             description=description,
+            # ingredient_name = ingredient_name,
+            # ingredient_quantity = ingredient_quantity,
+            # ingredient_measurement = ingredient_measurement,
+            # ingredient_seasoning = ingredient_seasoning,
+            # direction_step = direction_step,
+            # direction_step_info = direction_step_info,
+            prep_time = prep_time,
+            cook_time = cook_time,
+            servings = servings,
+            # ingredients=ingredients,
+            # directions=directions,
+            # time=time,
+            # preview_image=preview_image,
+            # recipe_image=recipe_image,
+            # description=description,
             )
         
-        for ingredient_data in ingredients:
-            ingredient = Ingredient.query.get(ingredient_data['id'])
-            
-            if ingredient is not None:
-                new_recipe_ingredient = RecipeIngredient(
-                    recipe = new_recipe,
-                    ingredient = ingredient,
-                    quantity = ingredient_data.get('quantity'),
-                    measurement = ingredient_data.get('measurement')
-                )
-                new_recipe.recipe_ingredients.append(new_recipe_ingredient)
-                
-        for direction_data in directions:
-            new_direction = Direction(
-                recipe = new_recipe,
-                step = direction_data.get('step'),
-                step_info = direction_data.get('step_info'),
-            )
-            new_recipe.directions.append(new_direction)
-            
+        preview_image = RecipeImage(url=preview_image_url,is_preview=True)
+        recipe_image = RecipeImage(url=recipe_image_url, is_preview=False)
+        
+        new_recipe.recipe_images.append(preview_image)
+        new_recipe.recipe_images.append(recipe_image)
+        
         db.session.add(new_recipe)
+        db.session.commit()
+        
+        # directions = form.directions.data
+        
+        # for direction_data in directions:
+        #     new_direction = Direction(
+        #         recipe_id=new_recipe.id,
+        #         step=direction_data.get('step'),
+        #         step_info=direction_data.get('step_info'),
+        #         created_at=datetime.now(),
+        #         updated_at=datetime.now()
+        #     )
+        #     db.session.add(new_direction)
+        
+        
+        for key, value in form.data.items():
+            if key.startswith('ingredient'):
+                ingredient_info = value.split(',')
+                new_ingredient = Ingredient(name=ingredient_info[0], quantity=ingredient_info[1],measurement=ingredient_info[2], is_seasoning=ingredient_info[3])
+                db.session.add(new_ingredient)
+            elif key.startswith('direction'):
+                direction_info = value.split(',')
+                new_direction = Direction(step=direction_info[0], step_info=direction_info[1])
+                db.session.add(new_direction)
+        
+        # for ingredient_data in ingredients:
+        #     ingredient = Ingredient.query.get(ingredient_data['id'])
+            
+        #     if ingredient is not None:
+        #         new_recipe_ingredient = RecipeIngredient(
+        #             recipe = new_recipe,
+        #             ingredient = ingredient,
+        #             quantity = ingredient_data.get('quantity'),
+        #             measurement = ingredient_data.get('measurement')
+        #         )
+        #         new_recipe.recipe_ingredients.append(new_recipe_ingredient)
+                
+        # for direction_data in directions:
+        #     new_direction = Direction(
+        #         recipe = new_recipe,
+        #         step = direction_data.get('step'),
+        #         step_info = direction_data.get('step_info'),
+        #     )
+        #     new_recipe.directions.append(new_direction)
+        
+        
+            
+        # db.session.add(new_recipe)
         db.session.commit()
         return jsonify(new_recipe.to_dict()), 200       
             
--- app/forms/recipe_form.py
+++ app/forms/recipe_form.py
@@ -1,28 +1,41 @@
 from flask import request
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, FormField, FieldList, SubmitField
+from wtforms import StringField, TextAreaField, IntegerField, BooleanField, FormField, FieldList, SubmitField
 from wtforms.validators import DataRequired
 
-from .direction_form import DirectionForm
-from .ingredient_form import IngredientForm
-from .recipe_image_form import RecipeImageForm
-from .time_form import TimeForm
+# from .direction_form import DirectionForm
+# from .ingredient_form import IngredientForm
+# from .recipe_image_form import RecipeImageForm
+# from .time_form import TimeForm
 
 class RecipeForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
-    ingredients = FieldList(FormField(IngredientForm), min_entries=1)
-    directions = FieldList(FormField(DirectionForm), min_entries=1)
-    time = FieldList(FormField(TimeForm), min_entries=1)
-    preview_image = StringField('Preview Image', validators=[DataRequired()])
-    recipe_image = FieldList(FormField(RecipeImageForm), min_entries=1)
-    description = TextAreaField('Description')
-    submit = SubmitField('Submit')
+    description = TextAreaField('Description', validators=[DataRequired()])
     
-    def __init__(self,*args,**kwargs):
-        data = request.get_json()
-        super(RecipeForm, self).__init__(data=data, *args, **kwargs)
-
-
-# ingredients_name = StringField('Name', validators=[DataRequired()])
-# ingredients_quantity = IntegerField('Quantity', validators=[DataRequired()])
-# ingredients_measurement = StringField('Measurement', validators=[DataRequired()])
+    ingredient_name = StringField('Name', validators=[DataRequired()])
+    ingredient_quantity = IntegerField('Quantity', validators=[DataRequired()])
+    ingredient_measurement = StringField('Measurement', validators=[DataRequired()])
+    ingredient_seasoning = BooleanField('Is Seasoning')
+    
+    direction_step = IntegerField('Step')
+    direction_step_info = StringField('Step Info', validators=[DataRequired()])
+    
+    prep_time = IntegerField('Prep Time', validators=[DataRequired()])
+    cook_time = IntegerField('Cook Time', validators=[DataRequired()])
+    servings = IntegerField('Servings', validators=[DataRequired()])
+    
+    preview_image = StringField('Preview Image', validators=[DataRequired()])
+    recipe_image = StringField('Recipe Image')
+    
+    # name = StringField('Name', validators=[DataRequired()])
+    # ingredients = FieldList(FormField(IngredientForm), min_entries=1)
+    # directions = FieldList(FormField(DirectionForm), min_entries=1)
+    # time = FieldList(FormField(TimeForm), min_entries=1)
+    # preview_image = StringField('Preview Image', validators=[DataRequired()])
+    # recipe_image = FieldList(FormField(RecipeImageForm), min_entries=1)
+    # description = TextAreaField('Description')
+    # submit = SubmitField('Submit')
+    
+    # def __init__(self,*args,**kwargs):
+    #     data = request.get_json()
+    #     super(RecipeForm, self).__init__(data=data, *args, **kwargs)
