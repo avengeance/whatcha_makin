e8fa9751e0c18942478134c083849ad2f7a54ad2
Calvin Le <email.calvinle@gmail.com>
2023-07-02 16:16:46 -0700

    update a review completed tested locally

--- app/api/recipe_routes.py
+++ app/api/recipe_routes.py
@@ -9,7 +9,7 @@ from ..models.like import Like
 from ..models.recipeIngredient import RecipeIngredient
 
 from ..forms.recipe_form import RecipeForm, EditRecipeForm
-from ..forms.review_form import ReviewForm
+from ..forms.review_form import ReviewForm, EditReviewForm
 
 from flask import Blueprint, redirect, url_for, render_template, jsonify, request
 from flask_login import login_required, current_user, logout_user
@@ -485,3 +485,40 @@ def create_review(id):
         return jsonify(new_review.to_dict()), 201
     else:
         return jsonify(form.errors), 400
+
+# Update a Review
+@recipe_routes.route('/<int:id>/reviews/<int:review_id>', methods=['PUT'])
+@login_required
+def update_review(id,review_id):
+    review = Review.query.get(review_id)
+    
+    if review is None:
+        return jsonify({
+            "error": "Review does not exist",
+            "status_code": 404
+        }), 404
+    
+    if review and current_user.id == review.owner_id:
+        form = EditReviewForm()
+        form['csrf_token'].data = request.cookies['csrf_token']
+        
+        if request.json:
+            form.review.data = request.json.get('review', None)
+            form.stars.data = request.json.get('stars', None)
+        
+        if form.validate_on_submit():
+            review.review = form.review.data
+            review.stars = form.stars.data
+            db.session.commit()
+            
+            review_dict = review.to_dict()
+            
+            return jsonify(review_dict), 200
+        else:
+            return jsonify(form.errors), 400
+    else:
+        return jsonify({
+            "message": "You do not have permission to update this review",
+            "status_code": 400
+        }), 400
+        
--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -5,3 +5,7 @@ from wtforms.validators import DataRequired
 class ReviewForm(FlaskForm):
     review = StringField('Review', validators=[DataRequired()])
     stars = IntegerField('Stars', validators=[DataRequired()])
+
+class EditReviewForm(FlaskForm):
+    review = StringField('Review')
+    stars = IntegerField('Stars')
