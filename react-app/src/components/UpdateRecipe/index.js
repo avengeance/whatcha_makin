30cda2493d295f0844ccf8c67c99f5914d483644
Calvin Le <email.calvinle@gmail.com>
2023-07-04 10:16:04 -0700

    completed update recipe comp

--- react-app/src/components/UpdateRecipe/index.js
+++ react-app/src/components/UpdateRecipe/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 
@@ -6,7 +6,7 @@ import * as RecipeActions from "./store/recipes";
 import { csrfFetch } from "../../store/csrf";
 import Cookies from "js-cookie";
 
-import "./CreateRecipe.css";
+import "./UpdateRecipe.css";
 
 const initialIngredient = {
     name: "",
@@ -14,16 +14,19 @@ const initialIngredient = {
     measurement: "",
     isSeasoning: false,
 }
+
 const initialDirection = {
     step: 0,
     stepInfo: "",
 }
 
-function CreateRecipe() {
+function UpdateRecipe() {
     const user = useSelector((state) => state.session.user);
     const dispatch = useDispatch();
     const history = useHistory();
+    const { id } = useParams();
 
+    const [recipe, setRecipe] = useState(null)
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
     const [ingredients, setIngredients] = useState([initialIngredient]);
@@ -36,16 +39,34 @@ function CreateRecipe() {
 
     const [errors, setErrors] = useState({});
 
+    useEffect(() => {
+        async function getRecipeThunk() {
+            const res = await csrfFetch(`/api/recipes/${id}`);
+            if (res.ok) {
+                const recipe = await res.json()
+                setRecipe(recipe)
+                setName(recipe.name)
+                setDescription(recipe.description)
+                setIngredients(recipe.ingredients)
+                setDirections(recipe.directions)
+                setPrepTime(recipe.prepTime)
+                setCookTime(recipe.cookTime)
+                setServings(recipe.servings)
+                setPreviewImage(recipe.image)
+                setRecipeImage(recipe.image)
+            }
+        }
+        getRecipeThunk();
+    }, [id])
+
     function handleIngredientChange(i, event) {
         const values = [...ingredients];
         values[i][event.target.name] = event.target.value
         setIngredients(values)
     }
-
     function handleAddIngredient() {
         setIngredients([...ingredients, { ...initialIngredient }])
     }
-
     function handleRemoveIngredient(i) {
         const values = [...ingredients];
         values.splice(i, 1);
@@ -70,7 +91,7 @@ function CreateRecipe() {
         e.preventDefault();
         setErrors({})
 
-        const formData = FormData()
+        const formData = new FormData()
 
         formData.append("name", name)
         formData.append("description", description)
@@ -92,8 +113,8 @@ function CreateRecipe() {
             formData.append(`directions[${index}].step_info`, direction.stepInfo)
         })
 
-        fetch('/api/recipes/new', {
-            method: "POST",
+        fetch(`/api/recipes/${id}`, {
+            method: "PATCH",
             body: formData
         })
             .then(response => response.json())
@@ -101,22 +122,21 @@ function CreateRecipe() {
             .catch(error => console.error(error))
 
         const payload = {
-            name,
-            description,
-            ingredients,
-            directions,
-            prepTime,
-            cookTime,
-            servings,
+            name: name,
+            description: description,
+            ingredients: ingredients,
+            directions: directions,
+            prepTime: prepTime,
+            cookTime: cookTime,
+            servings: servings,
+            previewImage: previewImage,
+            recipeImage: recipeImage
         }
-        const newRecipe = await dispatch(RecipeActions.createRecipeThunk(payload));
 
-        if (newRecipe) {
-            history.push(`/recipes/${newRecipe.id}`);
+        const updatedRecipe = await dispatch(RecipeActions.updateRecipeThunk(id, payload));
+
+        if(updatedRecipe) {
+            history.push(`/recipes/${updatedRecipe.id}`);
         }
     }
-
-    return (
-        <></>
-    )
 }
