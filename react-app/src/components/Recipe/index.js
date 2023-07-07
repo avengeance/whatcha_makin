6d884ba1db074353cdd38d66ee2775bd10dc1114
Calvin Le <email.calvinle@gmail.com>
2023-07-06 20:21:56 -0700

    main details of landing page done

--- react-app/src/components/Recipe/index.js
+++ react-app/src/components/Recipe/index.js
@@ -1,24 +1,76 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
-import * as RecipeActions from "../../store/recipes"
 import "./Recipe.css";
 
-function Recipe(){
+function Recipe() {
     const dispatch = useDispatch();
     const history = useHistory();
-    const recipes = useSelector((state) => Object.values(state.recipes.recipes));
+    const tooltipRef = useRef();
+    const recipe = useSelector((state) => Object.values(state.recipes));
+
+    const [recipes, setRecipes] = useState([]);
 
     useEffect(() => {
-        dispatch(RecipeActions.getAllRecipesThunk());
-    }, [dispatch]);
+        fetch("/api/recipes")
+            .then((res) => res.json())
+            .then((data) => {
+                const recipeArray = data.recipes
+                setRecipes(recipeArray)
+            })
+            .catch((error) => {
+                console.error("Error:", error)
+            })
+    }, [])
 
-    const handleRecipeClick = (recipeId) => {
-        history.push(`/recipes/${recipeId}`);
+    const tileHover = (e, recipeName) => {
+        const tooltip = tooltipRef.current
+        tooltip.textContent = recipeName
+        tooltip.style.display = 'block'
+
+        const x = e.clientX
+        const y = e.clientY
+
+        tooltip.style.top = `${y + 20}px`
+        tooltip.style.left = `${x + 10}px`
     }
+
     return (
-        <></>
+        <div className='recipe-tile-container'>
+            <div ref={tooltipRef} id='recipe-tooltip'></div>
+            {recipes.map(recipe => (
+                <div className='recipe-tile'
+                    key={recipe.id}
+                    onMouseEnter={(e) => tileHover(e, recipe.name)}
+                    onMouseOut={() => {
+                        const tooltip = document.getElementById('recipe-tooltip')
+                        tooltip.textContent = ''
+                    }}
+                    onClick={() => window.location.href = `/recipes/${recipe.id}`}>
+                    <img src={recipe.preview_image[0].url} alt={recipe.name} id='recipe-tile-image' />
+                    <div title={recipe.name}>
+                        <div className='recipe-review-likes-container'>
+                            <div id='recipe-name'>
+                                {recipe.name}
+                            </div>
+                            <div className='recipe-review-likes'>
+                                <div id='avgRating'>
+                                    <i className='fas fa-star'></i>
+                                    {recipe.avg_rating ? recipe.avg_rating.toFixed(1) : 'New'}
+                                </div>
+                                <div id='likes'>
+                                    {recipe.likes > 0
+                                        ? <><i className="fa-solid fa-heart"></i> {recipe.likes} </>
+                                        : <><i className="far fa-heart"></i> New</>}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            ))}
+        </div>
     )
 }
 
