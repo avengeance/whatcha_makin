3ef567f931b1ab94b3f9042da6bc8824818af692
Calvin Le <email.calvinle@gmail.com>
2023-07-08 11:50:03 -0700

    can view all reviews for recipe

--- react-app/src/components/RecipeDetail/index.js
+++ react-app/src/components/RecipeDetail/index.js
@@ -21,6 +21,7 @@ const RecipeDetail = () => {
     const history = useHistory();
 
     const currentRecipe = useSelector((state) => state.recipes.recipes[recipeId]);
+    const currentReviews = useSelector((state) => state.reviews.reviews);
     const ownerId = useSelector((state) => state.session.user?.id);
     const likesByRecipe = useSelector((state) => state.likes.likesByRecipe);
 
@@ -108,10 +109,12 @@ const RecipeDetail = () => {
                                     ))}
                                 </div>
                             </div>
+                        </div>
+                        <div className='recipe-description'>
                             <div className='recipe-description-time-container'>
                                 <div className='recipe-description-owner-container'>
                                     <div className='recipe-owner-name'>
-                                        <h2>{currentRecipes?.owner_name}</h2>
+                                        <h2>Recipe Created by: {currentRecipes?.owner_name}</h2>
                                     </div>
                                     <div className='recipe-description'>
                                         <p>{currentRecipes?.description}</p>
@@ -123,6 +126,56 @@ const RecipeDetail = () => {
                                     <h4>Cook Time - {currentRecipes?.cook_time} min</h4>
                                     <h3>Servings - {currentRecipes?.servings}</h3>
                                 </div>
+                            </div>
+                            <div className='recipe-ingredients-seasoning-containter'>
+                                <div className='recipe-ingredients'>
+                                    <h3>Ingredients</h3>
+                                    {currentRecipes?.ingredients &&
+                                        currentRecipes.ingredients
+                                            .filter(ingredient => !ingredient.is_seasoning)
+                                            .map((ingredient, i) => (
+                                                <p key={i}>
+                                                    {ingredient.quantity} {ingredient.measurement} {ingredient.name}
+                                                </p>
+                                            ))}
+                                </div>
+                                <div className='recipe-seasoning'>
+                                    <h3>Seasoning</h3>
+                                    {currentRecipes?.ingredients &&
+                                        currentRecipes.ingredients
+                                            .filter(ingredient => ingredient.is_seasoning)
+                                            .map((ingredient, i) => (
+                                                <p key={i}>
+                                                    {ingredient.quantity} {ingredient.measurement} {ingredient.name}
+                                                </p>
+                                            ))}
+                                </div>
+                            </div>
+                        </div>
+                        <div className='recipe-directions-container'>
+                            <div className='recipe-directions'>
+                                <h3>Directions</h3>
+                                {currentRecipes?.directions &&
+                                    currentRecipes.directions.map((direction, i) => (
+                                        <p key={i}>{direction.step}: {direction.step_info}</p>
+                                    ))}
+                            </div>
+                        </div>
+                        <div className='recipe-reviews-container'>
+                            <div className='recipe-reviews'>
+                                <h3>Reviews</h3>
+                                {currentReviews.map((review, i) => (
+                                    <div key={i} className='recipe-review-container'>
+                                        <div className='recipe-review-user'>
+                                            <div className='recipe-review-user-name'>
+                                                <h3>{review?.owner_name}</h3>
+                                                <p><i className='fas fa-star'></i>{review?.stars}</p>
+                                            </div>
+                                            <p id='review-created'>{review?.created_at}</p>
+                                        </div>
+                                        <p>{review?.review}</p>
+                                    </div>
+                                ))}
                             </div>
                         </div>
                     </div>
